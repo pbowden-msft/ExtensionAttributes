ca61fbb584ebdc55b106168f5500ade77bb37f12
Paul Bowden <pbowden@microsoft.com>
2021-03-04 07:45:50 -0800

    Script version of Zoom plug-in EA

---
+++ Zoom_Plug-In_Version.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+## Extension Attribute to report the version of the Zoom injection-based plug-in for Outlook
+
+if [ -d /Library/Application\ Support/ZoomOutlookPlugin/zOutlookPluginAgent.app ]; then
+    ZoomVersion=`/usr/bin/defaults read /Library/Application\ Support/ZoomOutlookPlugin/zOutlookPluginAgent.app/Contents/Info.plist CFBundleVersion`
+    echo "<result>$ZoomVersion</result>"
+elif [ -d /Users/Shared/ZoomOutlookPlugin/zOutlookPluginAgent.app ]; then
+    ZoomVersion=`/usr/bin/defaults read /Users/Shared/ZoomOutlookPlugin/zOutlookPluginAgent.app/Contents/Info.plist CFBundleVersion`
+    echo "<result>$ZoomVersion</result>"
+else
+    echo "<result>Not installed</result>"
+fi
