71789926dafa30bb1fa3a4acb6f8ec321e444268
Paul Bowden <pbowden@microsoft.com>
2021-04-08 15:23:02 -0700

    v2

--- MAU_Pending_Updates.sh
+++ MAU_Pending_Updates.sh
@@ -4,20 +4,6 @@
 
 autoload is-at-least
 
-GetLoggedInUser() {
-	LOGGEDIN=$(/bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/&&!/loginwindow/{print $3}')
-	if [ "$LOGGEDIN" = "" ]; then
-		echo "$USER"
-	else
-		echo "$LOGGEDIN"
-	fi
-}
-
-GetTempFolder() {
-	TEMPFOLDERPATH=$(/usr/bin/sudo -u "$1" /bin/echo "$TMPDIR")
-	echo "$TEMPFOLDERPATH"
-}
-
 CheckMAUVersion() {
 	if [ -d /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app ]; then
     	APPVERSION=$(/usr/bin/defaults read /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/Info.plist CFBundleShortVersionString)
@@ -31,13 +17,16 @@ CheckMAUVersion() {
 	fi
 }
 
-## Main
-LoggedInUser=$(GetLoggedInUser)
-TempFolder=$(GetTempFolder "$LoggedInUser")
-CheckMAUVersion
-CloneFolder="$TempFolder/MSauClones"
+GetCloneFolder() {
+	CLONEPATH=$(/usr/bin/find /var/folders -name 'MSauClones')
+	echo "$CLONEPATH"
+}
 
-# Look for the clone folder
+## Main
+CheckMAUVersion
+CloneFolder=$(GetCloneFolder)
+
+# Enumerate the apps in the clone folder
 if [ -d "$CloneFolder" ]; then
 	for app in $CloneFolder/**; do
 		APPNAME=$(defaults read "$app/Contents/Info" CFBundleName)
