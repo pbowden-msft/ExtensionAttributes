66ae271fd033fb327a704d1dbb1bb6d10fc61a22
Paul Bowden <pbowden@microsoft.com>
2021-04-15 12:28:44 -0700

    Fix for New Outlook binary parsing

--- Outlook_AddIns.sh
+++ Outlook_AddIns.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
+# set -x
 
-## Extension Attribute to report the list of web-based add-ins that Outlook has loaded
+## Extension Attribute to report the list of modern web-based add-ins that Outlook has loaded
 
 GetLoggedInUser() {
 	LOGGEDIN=$(/bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/&&!/loginwindow/{print $3}')
@@ -23,7 +24,8 @@ GetHomeFolder() {
 }
 
 GetManifestsFolder() {
-	MANIFESTPATH=$(/usr/bin/find "$HOME/Library/Containers/com.microsoft.Outlook/Data/Library/Application Support/Microsoft/Office/16.0/Wef" -name 'Manifests')
+	# Outlook can create multiple Manifests folders, so get the most recent
+	MANIFESTPATH=$(print -lr -- $HOME/Library/Containers/com.microsoft.Outlook/Data/Library/Application\ Support/Microsoft/Office/16.0/Wef/**/Manifests(om[1,1]))
 	echo "$MANIFESTPATH"
 }
 
@@ -35,7 +37,8 @@ ManifestsFolder=$(GetManifestsFolder)
 # Enumerate files in the Manifests folder
 if [ -d "$ManifestsFolder" ]; then
 	for manifest in $ManifestsFolder/**; do
-		ADDIN=$(cat "$manifest" | grep '<DisplayName' | cut -d '"' -f2)
+		# New Outlook uses binary-based manifests, so we need to remove non-text characters as part of the grep
+		ADDIN=$(cat "$manifest" | LC_ALL=C tr -d '[\000-\011\013-\037\177-\377]' | grep '<DisplayName' | cut -d '"' -f2 | sed -e 's/^[[:space:]]*//')
 		if [ "$ADDIN" != "" ]; then
 			AddInList+="$ADDIN;"
 		fi
