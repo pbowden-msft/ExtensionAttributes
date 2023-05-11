693e582625b3ac8d22372364e169c70ad194466f
Paul Bowden <pbowden@microsoft.com>
2023-05-11 12:35:13 -0400

    Added support for New Outlook

--- OutlookFolderSize.sh
+++ OutlookFolderSize.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/zsh
 #set -x
 
 TOOL_NAME="Microsoft Outlook Local Folders Size Extension Attribute"
-TOOL_VERSION="1.0"
+TOOL_VERSION="1.1"
 
-## Copyright (c) 2020 Microsoft Corp. All rights reserved.
+# There are two parts to the returned value. 
+#   'Legacy' refers to the total size of local messages in legacy (aka Classic) Outlook
+#   'New' refers to the total size of local messages in New Outlook
+# In some cases, both values may be greater than 0.00MB. This means that the user has local messages in both modes
+
+## Copyright (c) 2023 Microsoft Corp. All rights reserved.
 ## Scripts are not supported under any Microsoft standard support program or service. The scripts are provided AS IS without warranty of any kind.
 ## Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a 
 ## particular purpose. The entire risk arising out of the use or performance of the scripts and documentation remains with you. In no event shall
@@ -29,9 +34,10 @@ function DetermineOutlookProfile() {
 function DetermineOutlookDB() {
 	OUTLOOK_DB="/Users/${USERNAME}/Library/Group Containers/UBF8T346G9.Office/Outlook/Outlook 15 Profiles/${PROFILE_NAME}/Data/Outlook.sqlite"
 	OUTLOOK_FOLDER="/Users/${USERNAME}/Library/Group Containers/UBF8T346G9.Office/Outlook/Outlook 15 Profiles/${PROFILE_NAME}/Data/"
+	OUTLOOK_NEW_OMC="/Users/${USERNAME}/Library/Group Containers/UBF8T346G9.Office/Outlook/Outlook 15 Profiles/${PROFILE_NAME}/Omc/"
 }
 
-# Function to get the list of local messages from the Outlook database, and get the file size on disk
+# Function to get the list of local messages from the classic Outlook database, and then get the file size on disk
 function GetLocalMessages() {
 	OMCSIZE=0
 	local MSGRESULT=$(sqlite3 "${OUTLOOK_DB}" "SELECT PathToDataFile from Mail WHERE Record_AccountUID='0'")
@@ -45,11 +51,14 @@ function GetLocalMessages() {
 		MSGSIZE=$(/usr/bin/stat -qf%z "${OUTLOOK_FOLDER}${msg}")
 		OMCSIZE=$(($OMCSIZE + $MSGSIZE))
 	done
+	LEGACYSIZE_MB=$(/usr/bin/awk "BEGIN {printf \"%.2f\n\", ${OMCSIZE}/1000/1000}")
 }
 
-# Function to convert message size to megabytes
-function ConvertSizeToMB() {
-	FINALSIZE=$(/usr/bin/awk "BEGIN {printf \"%.2f\n\", ${OMCSIZE}/1000/1000}")
+# Function to get the list of local messages from New Outlook's Omc folders, and then get the file size on disk
+function GetNewOmcMessages() {
+	NEWOMCSIZE=0
+	NEWOMCSIZE=$(/usr/bin/find ${OUTLOOK_NEW_OMC} -iname '*.eml' -ls | awk '{total += $7} END {print total}')
+	NEWOMCSIZE_MB=$(/usr/bin/awk "BEGIN {printf \"%.2f\n\", ${NEWOMCSIZE}/1000/1000}")
 }
 
 ## MAIN
@@ -57,8 +66,8 @@ DetermineLoginState
 DetermineOutlookProfile
 DetermineOutlookDB
 GetLocalMessages
-ConvertSizeToMB
+GetNewOmcMessages
 
-/bin/echo "<result>${FINALSIZE} MB</result>"
+/bin/echo "<result>Legacy:${LEGACYSIZE_MB}MB / New:${NEWOMCSIZE_MB}MB</result>"
 
 exit 0
